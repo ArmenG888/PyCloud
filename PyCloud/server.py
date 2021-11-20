fe728e4448049d005c17553ffb13027dec913b1a
Armen._.G <52843550+ArmenG888@users.noreply.github.com>
2021-11-20 15:03:25 -0800

    Update server.py

--- PyCloud/server.py
+++ PyCloud/server.py
@@ -62,7 +62,7 @@ class server:
         file_list = self.conn.recv(1024).decode()
         file_list = file_list.split(",")
         print(file_list)
-        for i in range(lena(file_list)):
+        for i in range(len(file_list)):
             jsonString = bytearray()
             while True:
                 # recieves the packet by 1024
@@ -94,23 +94,5 @@ class server:
             self.conn.send("1".encode())
         else:
             self.conn.send("0".encode())
-def main(argv):
-    if argv[0] == "-h":
-        print("-startserver [ip](default 127.0.0.1) [port](default 52000), -backup (backups all the data of the users into one zip file)")
-    elif argv[0] == "-backup":
-        directories_in_curdir = list(filter(os.path.isdir, os.listdir(os.curdir)))
-        x = 0
-        for i in directories_in_curdir:
-            shutil.make_archive(i, 'zip',i)
-            x += 1
-            print(str(x)+"/"+str(len(directories_in_curdir)))
-    elif argv[0] == "-startserver":
-        if len(argv) == 2:
-            app = server(ip=argv[1],port=argv[1])
-        elif len(argv) == 1:
-            app = server(ip=argv[1],port=52000)
-        else:
-            print("The server is goint to run on the default test ip")
-            app = server(ip=argv[1],port=52000)
 if __name__ == "__main__":
-   main(sys.argv[1:])
+   server = server("127.0.0.1",52000)
